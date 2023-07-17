5e1bc4d3e4ed1b6e3990c6f7aaa52b3a11e0e917
Belkiss Lino Ferreira de Souza <belkiss.lino@gmail.com>
2023-07-17 18:21:49 -0400

    portfolio changes

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,7 @@ import './Portfolio.css';
 import IMG0 from '../../assets/portfolio0.png';
 import IMG1 from '../../assets/portfolio1.png';
 import IMG2 from '../../assets/portfolio2.png';
-import IMG3 from '../../assets/portfolio3.jpeg';
+import IMG3 from '../../assets/portfolio3.png';
 import IMG4 from '../../assets/portfolio4.jpeg';
 import IMG5 from '../../assets/portfolio5.jpg';
 
@@ -18,7 +18,7 @@ const Portfolio = () => {
           <div className='portfolio__item-image'>
             <img src={IMG0} alt="" />
           </div>
-          <h3>Loja de Livros - Com Redux</h3>
+          <h3>Loja de Livros - React/Redux</h3>
           <div className='portfolio__item-cta'> 
             <a href='https://github.com/belkiss-lino/book-shopping' className='btn' target='_blank'>GitHub</a>
           </div>
@@ -27,7 +27,7 @@ const Portfolio = () => {
           <div className='portfolio__item-image'>
             <img src={IMG1} alt="" />
           </div>
-          <h3>Previsão do Tempo - Consumindo API</h3>
+          <h3>Previsão do Tempo - REACT/Consumo API</h3>
           <div className='portfolio__item-cta'> 
             <a href='https://github.com/belkiss-lino/weather-forecast' className='btn' target='_blank'>GitHub</a>
           </div>
@@ -36,7 +36,7 @@ const Portfolio = () => {
           <div className='portfolio__item-image'>
             <img src={IMG2} alt="" />
           </div>
-          <h3>Landing Page</h3>
+          <h3>Landing Page - HTML/CSS</h3>
           <div className='portfolio__item-cta'> 
             <a href='https://github.com/belkiss-lino/landing-page-mabel' className='btn' target='_blank'>GitHub</a>
           </div>
@@ -45,7 +45,7 @@ const Portfolio = () => {
           <div className='portfolio__item-image'>
             <img src={IMG3} alt="" />
           </div>
-          <h3>App Loja de Tênis</h3>
+          <h3>Gerenciamento de Contas - HTML/CSS/JS</h3>
           <div className='portfolio__item-cta'> 
             <a href='https://github.com/belkiss-lino/AppLojaTenis' className='btn' target='_blank'>GitHub</a>
           </div>
@@ -54,7 +54,7 @@ const Portfolio = () => {
           <div className='portfolio__item-image'>
             <img src={IMG4} alt="" />
           </div>
-          <h3>App Finanças</h3>
+          <h3>App Finanças - React Native/Expo</h3>
           <div className='portfolio__item-cta'> 
             <a href='https://github.com/belkiss-lino/AppTelas' className='btn' target='_blank'>GitHub</a>
           </div>
@@ -63,7 +63,7 @@ const Portfolio = () => {
           <div className='portfolio__item-image'>
             <img src={IMG5} alt="" />
           </div>
-          <h3>Portfolio</h3>
+          <h3>Portfolio HTML/CSS/JS</h3>
           <div className='portfolio__item-cta'> 
             <a href='https://github.com/belkiss-lino/portfolio-belkiss' className='btn' target='_blank'>GitHub</a>
           </div>
